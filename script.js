9b271c2158b35980af1a354f1648bd1ff43f8672
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-11 11:05:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ function Person(name, age) {
     this.name = name;
     this.age = age;
 
-    this.greet = function() {
+
+}
+greet = function() {
         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
     };
-}
 
 function Employee(name, age, jobTitle) {
     Person.call(this, name, age);
     this.jobTitle = jobTitle;
 
-    this.jobGreet = function() {
+   
+}
+jobGreet = function() {
         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
     };
-}
 const person = new Person("Alice", 25);
 person.greet();
 const employee = new Employee("Bob", 30, "Manager");
